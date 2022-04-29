da11ca01c0d664099b75ce0d39d46d5456f1685b
Jacob Alamparambil <jacobsfo@gmail.com>
2022-04-29 14:30:00 -0700

    Update display.cs

--- Mystic Games/Assets/display.cs
+++ Mystic Games/Assets/display.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class display : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject Square;
+    public GameObject Canvas;
 
     // void Start()
     // {
