a8ea675bd7d68c81ca700edd0a7627a5c0ca4e3d
Jacob Alamparambil <jacobsfo@gmail.com>
2022-04-29 15:28:01 -0700

    stash commit

--- Mystic Games/Assets/display.cs
+++ Mystic Games/Assets/display.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class display : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject Canvas;
+    public GameObject Square;
 
     // void Start()
     // {
