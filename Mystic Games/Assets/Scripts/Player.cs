e95720194fe98a8c437bed50053c1ffd10a075c0
Arashdeep Singh <datonearash@gmail.com>
2022-05-26 13:51:48 -0700

    save/quit menu

--- Mystic Games/Assets/Scripts/Player.cs
+++ Mystic Games/Assets/Scripts/Player.cs
@@ -58,4 +58,24 @@ public class Player : MonoBehaviour
             LevelLoader.instance.LoadLevel("BattleArena");
         }
     }
+    
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        currentHealth = data.health;
+        currentMana = data.mana;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+
+        transform.position = position;
+    }
 }
