a182c5009f78fbbe52ac1946b9386bde0aa5485b
baxically <allythach379@gmail.com>
2022-05-27 13:05:39 -0700

    Update BattleSystem.cs
    revert

--- Mystic Games/Assets/Scripts/BattleSystem.cs
+++ Mystic Games/Assets/Scripts/BattleSystem.cs
@@ -83,10 +83,7 @@ public class BattleSystem : MonoBehaviour
     {
         dialogueText.text = enemyStat.charName + " attacks you!";
 
-        //Changed isDead directly to have stat type
         bool isDead = playerStat.TakeDamage(enemyStat.damage, enemyStat.type);
-        //Added GameObject.Find from scene perseverance code in attempt to fix conflicts
-        GameObject.FindWithTag("Player").GetComponent<CharacterCombatStatus>().TakeDamage(enemyStat.damage);
         playerHUD.SetHealth(playerStat.currHealth);
 
         yield return new WaitForSeconds(3f);
